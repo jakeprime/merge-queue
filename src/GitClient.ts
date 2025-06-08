ad1f048456e454828f02ccb0056c19bed9611866
Jake Prime <jake@jakeprime.com>
2025-06-08 16:19:17 +0100

    --wip-- [skip ci]

--- src/GitClient.ts
+++ src/GitClient.ts
@@ -1,30 +1,13 @@
 import * as core from '@actions/core'
-import { simpleGit, SimpleGit, SimpleGitOptions } from 'simple-git'
+import { simpleGit, SimpleGit } from 'simple-git'
 import Env from './Env.js'
-import { wait } from './wait.js'
 
 export default class {
   init = async () => {
-    core.info('Initing Git 1')
-    core.info(`GH_TOKEN = (${Env.githubToken.length})`)
-    await wait(5000)
+    core.info('Initing Git')
 
-    const dir = `${Env.workingDir}/merge-queue`
-    // fs.rmSync(dir, { force: true, recursive: true })
-    // fs.mkdirSync(dir, { recursive: true })
-
-    const options: Partial<SimpleGitOptions> = { baseDir: dir }
-
-    // when setting all options in a single object
-    const git: SimpleGit = simpleGit(options)
-    // await git.cwd(dir)
-    // await git.init()
-    // await git.addRemote(
-    //   'origin',
-    //   `https://${Env.githubToken}@github.com/jakeprime/merge-subject`
-    // )
-    // await git.fetch('origin', 'main', { '--depth': 1 })
-    // await git.pull('origin', 'main')
+    const baseDir = `${Env.workingDir}/merge-queue`
+    const git: SimpleGit = simpleGit({ baseDir })
     const logOutput = await git.log()
     const message = logOutput.all[0].message
     core.info(message)
