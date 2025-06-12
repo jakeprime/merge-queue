6d4cf139b8e43d589cb236c73b460cf7726680e4
Jake Prime <jake@jakeprime.com>
2025-06-12 20:14:20 +0100

    Add Comment class

--- lib/comment.rb
+++ lib/comment.rb
@@ -13,15 +13,24 @@ class Comment
   end
 
   def message(message, init: false)
-    puts "User is #{client.user.login}"
+    if init
+      result = client.add_comment(project_repo, pr_number, message)
+      self.comment_id = result.id
+    else
+      client.update_comment(project_repo, comment_id, message)
+    end
   end
 
   private
+
+  attr_accessor :comment_id
 
   def client
     Octokit::Client.new(access_token:)
   end
   memoize :client
 
-  def access_token = ENV.fetch('GITHUB_TOKEN')
+  def access_token = ENV.fetch('ACCESS_TOKEN')
+  def pr_number = ENV.fetch('PR_NUMBER')
+  def project_repo = ENV.fetch('GITHUB_REPOSITORY')
 end
