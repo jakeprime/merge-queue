9299578d667278ca25347c500530615c10f06a8d
Jake Prime <jake@jakeprime.com>
2025-06-12 22:16:29 +0100

    Add logging

---
+++ lib/github_logger.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class GithubLogger
+  def self.log(message, level: :info)
+    # The ::level:: format is respected by Github actions
+    # By default `debug` logs will not be output, but will be if debug mode is
+    # enable on the run
+    puts "::#{level}:: #{message}"
+  end
+
+  def self.debug(message)
+    log(message, level: :debug)
+  end
+
+  def self.info(message)
+    log(message, level: :info)
+  end
+
+  def self.warn(message)
+    log(message, level: :warn)
+  end
+
+  def self.error(message)
+    log(message, level: :error)
+  end
+end
--- lib/merge_queue.rb
+++ lib/merge_queue.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 require_relative './comment'
+require_relative './github_logger'
 
 class MergeQueue
   def call
-    Comment.init('Initial commit')
+    create_initial_comment
+  end
+
+  private
+
+  def create_initial_comment
+    GithubLogger.debug('Creating initial comment')
+
+    Comment.init('🌱 Initialising merging process...')
   end
 end
