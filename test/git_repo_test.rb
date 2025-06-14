634c27f63ed73a523667b86f0c0e272074f02c5f
Jake Prime <jake@jakeprime.com>
2025-06-14 17:54:47 +0100

    Checkout repositories
    We want to pull as little as possible and then extend the fetch depth as and
    when needed. A large repo will be 100s of MB which will take time, and
    potentially hit disk limits.

--- test/git_repo_test.rb
+++ test/git_repo_test.rb
@@ -9,6 +9,8 @@ class GitRepoTest < Minitest::Test
     # prevent leakage of state between tests
     GitRepo.repos = {}
     FileUtils.stubs(:mkdir_p)
+
+    stub_git
   end
 
   def test_init_only_creates_one_repo_with_name
@@ -21,6 +23,38 @@ class GitRepoTest < Minitest::Test
     FileUtils.unstub(:mkdir_p)
     FileUtils.expects(:mkdir_p).with("#{WORKSPACE_DIR}/name")
 
-    git_repo = GitRepo.init(name: 'name', repo: 'repo')
+    GitRepo.init(name: 'name', repo: 'repo')
+  end
+
+  def test_initializes_git
+    Git.unstub(:init)
+    Git.expects(:init).with("#{WORKSPACE_DIR}/name").returns(git)
+
+    GitRepo.init(name: 'name', repo: 'repo')
+  end
+
+  def test_checkout_main
+    git.expects(:add_remote).with('origin', 'https://github.com/repo')
+    git.expects(:fetch).with('origin', depth: 1, ref: 'main')
+    git.expects(:checkout).with('main')
+
+    GitRepo.init(name: 'name', repo: 'repo')
+  end
+
+  def test_checkout_branch
+    git.expects(:add_remote).with('origin', 'https://github.com/repo')
+    git.expects(:fetch).with('origin', depth: 1, ref: 'branch')
+    git.expects(:checkout).with('branch')
+
+    GitRepo.init(name: 'name', repo: 'repo', branch: 'branch')
+  end
+
+  private
+
+  attr_reader :git
+
+  def stub_git
+    @git = stub(add_remote: nil, fetch: nil, checkout: nil)
+    Git.stubs(:init).returns(git)
   end
 end
