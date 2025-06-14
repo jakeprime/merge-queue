d76b3c5f3cc172c986cdc6ad429c26a35deec656
Jake Prime <jake@jakeprime.com>
2025-06-14 16:23:28 +0100

    Class to manage the PullRequest
    Currently this only initialises the branch names, but later it can manage the
    merging too, via a GitRepo object.

--- test/pull_request_test.rb
+++ test/pull_request_test.rb
@@ -9,41 +9,59 @@ class PullRequestTest < Minitest::Test
     @title = 'title'
 
     stub_octokit
+
+    @pull_request = PullRequest.new
   end
 
   def test_branch_name
-    pull_request = PullRequest.new
-
     assert_equal branch_name, pull_request.branch_name
   end
 
   def test_mergeable
-    pull_request = PullRequest.new
-
     assert_predicate pull_request, :mergeable?
   end
 
   def test_rebaseable
-    pull_request = PullRequest.new
-
     assert_predicate pull_request, :rebaseable?
   end
 
   def test_sha
-    pull_request = PullRequest.new
-
     assert_equal sha, pull_request.sha
   end
 
   def test_title
-    pull_request = PullRequest.new
-
     assert_equal title, pull_request.title
+  end
+
+  def test_init_merge_branches_set_base_branch
+    stub_queue_state(latest_merge_branch: nil)
+
+    pull_request.init_merge_branches
+
+    assert_equal 'main', pull_request.base_branch
+  end
+
+  def test_init_merge_branches_set_merge_branch
+    stub_queue_state(branch_counter!: 2)
+
+    pull_request.init_merge_branches
+
+    assert_equal 'merge-branch/title-2', pull_request.merge_branch
   end
 
   private
 
-  attr_reader :branch_name, :octokit, :sha, :title
+  attr_reader :branch_name, :queue_state, :octokit, :pull_request, :sha, :title
+
+  def stub_queue_state(**params)
+    stubs = {
+      branch_counter!: 1,
+      latest_merge_branch: nil,
+    }.merge(params)
+
+    @queue_state = stub(**stubs)
+    QueueState.stubs(:new).returns(queue_state)
+  end
 
   def stub_octokit
     head = stub(ref: branch_name, sha:)
