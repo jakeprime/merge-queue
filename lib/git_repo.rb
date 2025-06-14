5427eda95b2a2362f6cb37f6eb64133ff451f937
Jake Prime <jake@jakeprime.com>
2025-06-14 16:19:25 +0100

    Add GitRepo class
    We will need two working repos one for merging the PR and one to manage the
    queue state. This class keeps the state of each and ensures a single instance of
    each.

---
+++ lib/git_repo.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class GitRepo
+  # We only want to init a repo once, and then be able to access it at any time,
+  # so keep a persistent list of them
+  @@repos = {}
+
+  def self.init(name:, repo:, branch: 'main')
+    return find(name:) if find(name:)
+
+    @@repos[name] = new(name:, repo:, branch:)
+  end
+
+  def self.find(name:)
+    @@repos[name]
+  end
+
+  def initialize(name:, repo:, branch: 'main')
+    @name = name
+    @repo = repo
+    @branch = branch
+
+    checkout
+  end
+
+  private
+
+  attr_reader :branch, :name, :repo
+
+  def working_dir = File.join(workspace_dir, name)
+
+  def checkout
+    FileUtils.mkdir_p working_dir
+  end
+
+  def workspace_dir = ENV.fetch('GITHUB_WORKSPACE')
+end
