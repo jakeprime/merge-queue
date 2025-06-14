25240e6bcb007b751e8dbf50eb30622d1e8dcfad
Jake Prime <jake@jakeprime.com>
2025-06-14 16:23:28 +0100

    Class to manage the PullRequest
    Currently this only initialises the branch names, but later it can manage the
    merging too, via a GitRepo object.

--- lib/pull_request.rb
+++ lib/pull_request.rb
@@ -5,6 +5,8 @@ class PullRequest
   extend Forwardable
   include Memery
 
+  attr_reader :base_branch, :merge_branch
+
   def initialize
     @result = octokit.pull(project_repo, pr_number)
   end
@@ -14,9 +16,19 @@ class PullRequest
   def branch_name = result.head.ref
   def sha = result.head.sha
 
+  def init_merge_branches
+    @base_branch = queue_state.latest_merge_branch || 'main'
+
+    branch_counter = queue_state.branch_counter!
+    @merge_branch = "merge-branch/#{title}-#{branch_counter}"
+  end
+
   private
 
   attr_reader :result
+
+  def queue_state = QueueState.new
+  memoize :queue_state
 
   def octokit = Octokit::Client.new(access_token:)
   memoize :octokit
