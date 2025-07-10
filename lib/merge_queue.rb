5d94d8888aa19097fef5a9a3ff43e2a4362ed1d3
Jake Prime <jake@jakeprime.com>
2025-07-10 20:31:57 +0100

    Retry up to 3 times for retriable errors

--- lib/merge_queue.rb
+++ lib/merge_queue.rb
@@ -4,14 +4,21 @@ require_relative './merge_queue/merge_queue'
 
 module MergeQueue
   def self.call
-    merge_queue = MergeQueue.new.configure do |config|
-      config.access_token = ENV['ACCESS_TOKEN']
-      config.pr_number = ENV['PR_NUMBER']
-      config.project_repo = ENV['GITHUB_REPOSITORY']
-      config.run_id = ENV['GITHUB_RUN_ID']
-      config.workspace_dir = ENV['GITHUB_WORKSPACE']
-    end
+    retry_attempts = ENV.fetch('RETRY_ATTEMPTS', 3)
 
-    merge_queue.call
+    retry_attempts.times do
+      merge_queue = MergeQueue.new.configure do |config|
+        config.access_token = ENV['ACCESS_TOKEN']
+        config.pr_number = ENV['PR_NUMBER']
+        config.project_repo = ENV['GITHUB_REPOSITORY']
+        config.run_id = ENV['GITHUB_RUN_ID']
+        config.workspace_dir = ENV['GITHUB_WORKSPACE']
+      end
+
+      merge_queue.call
+      break
+    rescue RetriableError
+      # Try the whole process again
+    end
   end
 end
---
+++ test/lib/merge_queue_test.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+require_relative '../../lib/merge_queue'
+require_relative '../../lib/merge_queue/errors'
+
+class MergeQueueTest < Minitest::Test
+  def setup
+    @merge_queue = stub('MergeQueue')
+    merge_queue.stubs(:configure).returns(merge_queue)
+    MergeQueue::MergeQueue.expects(:new).returns(merge_queue)
+  end
+
+  attr_reader :merge_queue, :entrypoint
+
+  def test_merge_queue_runs_once_without_errors
+    merge_queue.expects(:call).once
+
+    MergeQueue.call
+  end
+
+  def test_merge_queue_runs_once_with_error
+    merge_queue.expects(:call).raises(MergeQueue::Error).once
+
+    assert_raises ::MergeQueue::Error do
+      MergeQueue.call
+    end
+  end
+
+  def test_merge_queue_retries_on_retriable_error
+    merge_queue.expects(:call).once
+
+    failing_merge_queue = stub('MergeQueueFailing').tap do
+      it.stubs(:configure).returns(it)
+      it.expects(:call).raises(MergeQueue::RetriableError).once
+    end
+
+    MergeQueue::MergeQueue.unstub(:new)
+    MergeQueue::MergeQueue.expects(:new).returns(failing_merge_queue, merge_queue).twice
+
+    MergeQueue.call
+  end
+end
